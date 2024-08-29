55f805e11c91e339ff149b040fffed4b033e2fa0
Tinno <tinnofabregas01@gmail.com>
2024-08-29 11:21:40 +0700

    add exception not found

--- app/Services/BlogService.php
+++ app/Services/BlogService.php
@@ -2,13 +2,16 @@
 
 namespace App\Services;
 
+use App\Exceptions\NotFoundException;
 use App\Models\Blog;
 
 class BlogService
 {
     public function getComments($id)
     {
-        $blog = Blog::findOrFail($id);
+        $blog = Blog::find($id);
+
+        if (!$blog) throw new NotFoundException();
 
         return $blog->comments();
     }
